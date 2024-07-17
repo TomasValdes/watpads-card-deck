b8e54ef4d276d5be9afd72fd76d58af0d8ded69d
Tomas Valdes <tvaldes60@gmail.com>
2024-07-16 21:49:32 -0500

    project set up

---
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/WatpadsCardDeckApplication.kt
@@ -0,0 +1,11 @@
+package com.sordle.WatpadsCardDeck
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WatpadsCardDeckApplication
+
+fun main(args: Array<String>) {
+	runApplication<WatpadsCardDeckApplication>(*args)
+}
---
+++ src/test/kotlin/com/sordle/WatpadsCardDeck/WatpadsCardDeckApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sordle.WatpadsCardDeck
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WatpadsCardDeckApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
