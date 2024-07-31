2400d82eb3fba095a3635865c81b2a7a10a25e61
Caleb Kugel <calebkugel1@gmail.com>
2024-07-30 23:21:13 -0500

    Added documentation to the game
    Still needs flip card logic

--- src/main/kotlin/com/sordle/WatpadsCardDeck/BotGame.java
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/BotGame.java
@@ -10,14 +10,15 @@ public class BotGame {
   private final ArrayList<CARDS> cardOptions = new ArrayList<>(Arrays.asList(CARDS.ROCK, CARDS.PAPER, CARDS.SCISSORS);
   private final short HAND_SIZE = 3;
   private final short CARDS_TO_REVEAL = 2;
-  private final Random random = new Random();
-  private Player player;
-  private Player bot;
-  private LinkedList<CARDS> startingDeck;
-  private LinkedList<CARDS> currentDeck;
-  private boolean revealTopCard = false;
+  private final Random random;
+  private Player player; // the object of the player playing on the site
+  private Player bot; // the object of the bot (completely controlled in this class
+  private LinkedList<CARDS> startingDeck; // 
+  private LinkedList<CARDS> currentDeck; // the deck after cards have been split up (gets cloned off of starting deck)
+  
+  private Player revealCardTo = null; // dictate the player to reveal the top two cards to
 
-  private GAME_STATE state;
+  private GAME_STATE state; // the current state of the game
 
   private short cardCounter = 0;
 
@@ -27,13 +28,38 @@ public class BotGame {
     {RESULT.P2, RESULT.P1, RESULT.TIE},
   };
 
+  /**
+   * @param p1 the player who is playing
+   * Creates a new game between a player and a bot
+   */ 
   public BotGame(Player p1) {
     player = p1;
     bot = new Player();
     startingDeck = new LinkedList<>(cardOptions);
     state = CARD_DRAFT;
+    random = new Random();
   }
 
+  /**
+   * @param p1 the player who is playing
+   * @param rand the random number generator to be used.
+   * SHOULD ONLY BE USED FOR DEBUG.
+   * Controllable game that is repeatable for testing purposes.
+   * Constructs debugging game between player and a bot.
+   */ 
+  protected BotGame(Player p1, Random rand) {
+    player = p1;
+    bot = new Player();
+    startingDeck = new LinkedList<>(cardOptions);
+    state = CARD_DRAFT;
+    random = rand;
+  }
+
+  /**
+   * @param card the card selected by the user
+   * Drafts the card into the starting deck then has the bot do so as well.
+   * After the bot drafts its cards, the cards are distributed and the game state changes to playing cards
+   */ 
   public void playerDraftCard(CARDS card) {
     startingDeck.add(card);
     cardCounter++;
@@ -46,6 +72,9 @@ public class BotGame {
 
   }
 
+  /**
+   * distributes the cards between the players at random
+   */ 
   private void distributeCards() {
     Collections.shuffle(startingDeck);
 
@@ -58,7 +87,7 @@ public class BotGame {
   }
 
   /**
-   * Determines the winner of the game and is dependent on win condition
+   * Determines the winner of the game and is dependent on win condition.
    * @return the winner of the game and null if neither player has won yet. updates game state if appropriate to do so
    */ 
   public Player getWinner() {
@@ -73,26 +102,42 @@ public class BotGame {
    return null;
   }
 
-  public RESULT getWinner(Player p1, Player p2) {
+  /**
+   * @param p1 player 1 relective in {@link #RESULT}
+   * @param p2 player 2 reflective in {@link #RESULT}
+   * Determines the winner of the standard game without taking into account win conditions
+   * @return the winner according to the decision matrix as a result reflecting the order of the parameters
+   */ 
+  private RESULT getWinner(Player p1, Player p2) {
     return movesToResult[p1.move][p2.move];
   } 
 
+  /**
+   * Drafts the cards from the bot randomly
+   */ 
   private void draftCardBot() {
     for(int i = 0; i < HAND_SIZE; i++) {
 	startingDeck.add(cardOptions.get(random.nextInt(cardOptions.size())));
     }
   }
 
-  public playCard(int index) {
+  /**
+   * @param index the index of the card in the players hand to remove
+   * plays the card and removes it from the player's hand
+   */ 
+  public void playCard(int index) {
      player.move = player.hand.remove(index);
   }
 
+  /**
+   * @return the options of what cards you can select from
+   */
   public List<CARDS> getDraftOptions() {
     return cardOptions;
   }
 
   public void play_again() {
-    // put data in db here
+    // update some sort of score in db here as well as games played
 
     state = GAME_STATE.CARD_DRAFT;
   }
