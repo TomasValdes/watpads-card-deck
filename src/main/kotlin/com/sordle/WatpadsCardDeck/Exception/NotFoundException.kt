7cf3f4d185ea8e5b1f22730369ac8db4370d0e39
Tomas Valdes <tvaldes@wisc.edu>
2024-08-02 15:33:12 -0500

    Changed UserNotFoundException.kt to NotFoundException.kt and removed the ExceptionHandler.kt with a base ApplicationException.kt config

--- src/main/kotlin/com/sordle/WatpadsCardDeck/Exception/NotFoundException.kt
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/Exception/NotFoundException.kt
@@ -5,4 +5,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-class UserNotFoundException(message: String) : RuntimeException(message)
+class NotFoundException(
+    statusCode: Int = 404,
+    errorMessage: String
+) : ApplicationException(statusCode, errorMessage)
