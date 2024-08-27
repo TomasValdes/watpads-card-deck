819685a2b972d717e8b36d1e693a454c1aa73768
Tomas Valdes <tvaldes@wisc.edu>
2024-08-27 15:53:35 -0500

    Player is an Entity, allow trumpCard to be null

--- src/main/kotlin/com/sordle/WatpadsCardDeck/entity/Player.kt
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/entity/Player.kt
@@ -7,17 +7,13 @@ import java.io.Serializable
 /**
  * Entity that represents player in a game.
  * Each player has a single game and user they are associated with.
- * Primary key is (gameId, userId)
  */
 @Embeddable
 data class Player (
-    @ManyToOne(optional = false, cascade = [CascadeType.ALL])
-    @JoinColumn(name = "user_id", insertable=false, updatable=false)
-    val user: User = User(),
+    val userId: Long = 0,
 
     @ElementCollection
     val playerHand: MutableList<Cards> = mutableListOf(),
 
-    // Defaults to rock, should be reassigned later
-    val trumpCard: Cards = Cards.Rock
+    val trumpCard: Cards? = null
 )
