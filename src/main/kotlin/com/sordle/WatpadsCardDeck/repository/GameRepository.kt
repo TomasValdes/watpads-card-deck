4b0e7c886382607015fa1da5381f412d39739097
Tomas Valdes <tvaldes@wisc.edu>
2024-08-27 14:11:51 -0500

    Add query to find open games

--- src/main/kotlin/com/sordle/WatpadsCardDeck/repository/GameRepository.kt
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/repository/GameRepository.kt
@@ -1,8 +1,9 @@
 package com.sordle.watpadsCardDeck.repository
 
 import com.sordle.watpadsCardDeck.entity.Game
+import com.sordle.watpadsCardDeck.entity.GameStates
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface GameRepository : JpaRepository<Game, Long> {
-
+    fun findByGameStateOrderByCreatedDateAsc(gameState: GameStates): List<Game>
 }
