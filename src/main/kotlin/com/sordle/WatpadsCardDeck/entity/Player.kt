b6da9675be1e4c6c09ebe2ad5bcc0ae186c066b8
Tomas Valdes <tvaldes@wisc.edu>
2024-08-27 14:09:58 -0500

    Set Player to an embedded object

--- src/main/kotlin/com/sordle/WatpadsCardDeck/entity/Player.kt
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/entity/Player.kt
@@ -4,25 +4,15 @@ import com.sordle.watpadsCardDeck.model.Cards
 import jakarta.persistence.*
 import java.io.Serializable
 
-@Embeddable
-data class PlayerId(
-    val gameId: Long = 0,
-    val userId: Long = 0
-) : Serializable
-
 /**
  * Entity that represents player in a game.
  * Each player has a single game and user they are associated with.
  * Primary key is (gameId, userId)
  */
-@Entity
+@Embeddable
 data class Player (
-    @EmbeddedId
-    val id: PlayerId = PlayerId(),
-
-    @ManyToOne(optional = false)
-    @MapsId("userId")
-    @JoinColumn(name = "user_id")
+    @ManyToOne(optional = false, cascade = [CascadeType.ALL])
+    @JoinColumn(name = "user_id", insertable=false, updatable=false)
     val user: User = User(),
 
     @ElementCollection
