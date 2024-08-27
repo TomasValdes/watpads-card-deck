4e02b1d6592c85766153e5895cea5fe2b86e0d37
Tomas Valdes <tvaldes@wisc.edu>
2024-08-27 15:52:41 -0500

    Remove redundant brackets

--- src/main/kotlin/com/sordle/WatpadsCardDeck/repository/UserRepository.kt
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/repository/UserRepository.kt
@@ -3,6 +3,4 @@ package com.sordle.watpadsCardDeck.repository
 import com.sordle.watpadsCardDeck.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, Long> {
-
-}
+interface UserRepository : JpaRepository<User, Long>
