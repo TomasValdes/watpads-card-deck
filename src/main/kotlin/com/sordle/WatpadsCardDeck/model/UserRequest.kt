e497b7a04aae60367be2be1a8ee091b22f478619
Tomas Valdes <tvaldes@wisc.edu>
2024-08-27 14:12:08 -0500

    Remove JSON naming property

--- src/main/kotlin/com/sordle/WatpadsCardDeck/model/UserRequest.kt
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/model/UserRequest.kt
@@ -1,11 +1,8 @@
 package com.sordle.watpadsCardDeck.model
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.annotation.JsonNaming
 import com.sordle.watpadsCardDeck.entity.User
 import jakarta.validation.constraints.NotBlank
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class UserRequest (
     @get:NotBlank
     val userName: String
