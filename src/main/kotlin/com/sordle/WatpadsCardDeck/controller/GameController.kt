541bc3ba76d8814c0b8a9651e894f249a2bd3737
Tomas Valdes <tvaldes@wisc.edu>
2024-08-28 16:47:25 -0500

    Switched from joining a game to finding a game

--- src/main/kotlin/com/sordle/WatpadsCardDeck/controller/GameController.kt
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/controller/GameController.kt
@@ -1,23 +1,19 @@
 package com.sordle.watpadsCardDeck.controller
 
-import com.sordle.watpadsCardDeck.model.GameRequest
+import com.sordle.watpadsCardDeck.model.GameResponse
 import com.sordle.watpadsCardDeck.service.GameService
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import jakarta.validation.Valid
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.GetMapping
 
 @RestController
 @RequestMapping("game")
 class GameController(
         private val gameService: GameService
 ){
-    @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE])
-    fun joinGame( @RequestBody @Valid gameRequest: GameRequest): ResponseEntity<String>{
-        gameService.joinGame(gameRequest)
-        return ResponseEntity.status(200).build()
+    @GetMapping()
+    fun findGame(): ResponseEntity<GameResponse>{
+        return ResponseEntity.ok(gameService.findGame())
     }
 }
