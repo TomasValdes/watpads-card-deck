1fd23bce15320eca6c35fe4d6edba0c61cba9036
Tomas Valdes <tvaldes@wisc.edu>
2024-08-28 16:58:34 -0500

    Added custom websocket handler

---
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/controller/GameWebSocketHandler.kt
@@ -0,0 +1,33 @@
+package com.sordle.watpadsCardDeck.controller
+
+import com.sordle.watpadsCardDeck.service.GameService
+import com.sordle.watpadsCardDeck.service.MessageService
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.handler.TextWebSocketHandler
+import org.springframework.web.socket.WebSocketSession
+
+@Component
+class GameWebSocketHandler (
+    private val gameService: GameService,
+    private val messageService: MessageService
+) : TextWebSocketHandler() {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    override fun handleTransportError(session: WebSocketSession, throwable: Throwable) {
+        logger.info(
+            "A transport error occurred for user with session id ${session.id}. Error Message: ${throwable.localizedMessage}")
+        throw throwable
+    }
+
+    override fun afterConnectionEstablished(session: WebSocketSession) {
+        gameService.joinGame(session)
+        logger.info("User with session id ${session.id} connected")
+    }
+
+    override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
+        logger.info("Received message ${message.payload}")
+        messageService.handleMessage(session, message.toString())
+    }
+}
