ca3eebbe50727d3cee235ed7dadf129b76876405
Tomas Valdes <tvaldes@wisc.edu>
2024-08-28 16:59:06 -0500

    Created util file for websocket sessions

---
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/util/WebSocketExtensions.kt
@@ -0,0 +1,28 @@
+package com.sordle.watpadsCardDeck.util
+
+import org.slf4j.LoggerFactory
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketSession
+import com.google.gson.Gson
+
+private val logger = LoggerFactory.getLogger("com.sordle.watpadsCardDeck.controller.GameWebSocketHandler")
+
+val WebSocketSession.gameId: Long
+    get() = attributes["gameId"] as Long
+
+val WebSocketSession.userId: Long
+    get() = attributes["userId"] as Long
+
+fun WebSocketSession.sendObjectMessage(obj: Any) {
+    // Might be more efficient to have a project wide deserializer
+    sendStringMessage(Gson().toJson(obj))
+}
+
+fun WebSocketSession.sendStringMessage(msg: String) {
+    try {
+        sendMessage(TextMessage(msg))
+    } catch (e: Exception) {
+        val shortenedMsg = if (msg.length > 40) msg.substring(0, 40) + "..." else msg
+        logger.error("Failed to send message '{}' to {}: {}", shortenedMsg, id, e.toString())
+    }
+}
