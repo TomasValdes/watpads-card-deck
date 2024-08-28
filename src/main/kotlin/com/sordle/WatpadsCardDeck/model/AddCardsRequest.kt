5b1dcbd0d3f28611ab5a5e384a763dc124c6197c
Tomas Valdes <tvaldes@wisc.edu>
2024-08-28 16:59:37 -0500

    Added request to be sent by websockets

---
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/model/AddCardsRequest.kt
@@ -0,0 +1,4 @@
+package com.sordle.watpadsCardDeck.model
+
+class AddCardsRequest {
+}
---
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/model/PlayCardRequest.kt
@@ -0,0 +1,5 @@
+package com.sordle.watpadsCardDeck.model
+
+data class PlayCardRequest (
+    val card : Cards
+)
