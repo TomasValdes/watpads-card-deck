8c00e9b3b2bafaa7ca791d70894549409678b235
Tomas Valdes <tvaldes@wisc.edu>
2025-02-05 16:48:54 -0600

    Added Syntax comments to PlayCardRequest and AddCardsRequest

--- src/main/kotlin/com/sordle/WatpadsCardDeck/model/PlayCardRequest.kt
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/model/PlayCardRequest.kt
@@ -1,14 +1,19 @@
 package com.sordle.watpadsCardDeck.model
 
 import com.google.gson.JsonObject
+import jakarta.validation.constraints.NotBlank
 
 /**
  * Message sent in the play card phase to play provided card
+ *
+ * Syntax: {"card" : "[card]"}
  */
 data class PlayCardRequest (
+    @get:NotBlank
     val card : Cards
 ) {
     constructor(jsonObject: JsonObject) : this(
         card = Cards.valueOf(jsonObject.get("card").asString)
     )
+
 }
