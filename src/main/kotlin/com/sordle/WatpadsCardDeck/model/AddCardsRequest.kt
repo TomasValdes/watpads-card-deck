191ee5cda330a97de5ebb8dfd352b3ca0731fd53
Tomas Valdes <tvaldes@wisc.edu>
2025-02-05 17:08:01 -0600

    Removed jakarta validation

--- src/main/kotlin/com/sordle/WatpadsCardDeck/model/AddCardsRequest.kt
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/model/AddCardsRequest.kt
@@ -1,7 +1,6 @@
 package com.sordle.watpadsCardDeck.model
 
 import com.google.gson.JsonObject
-import jakarta.validation.constraints.NotBlank
 
 /**
  * Message sent in the drafting cards phase to add to starting deck
@@ -9,7 +8,6 @@ import jakarta.validation.constraints.NotBlank
  * Syntax: {"card" : [[card]]}
  */
 data class AddCardsRequest (
-    @get:NotBlank
     val cardsToAdd : List<Cards>
 ){
     constructor(jsonObject: JsonObject) : this(
--- src/main/kotlin/com/sordle/WatpadsCardDeck/model/PlayCardRequest.kt
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/model/PlayCardRequest.kt
@@ -1,7 +1,6 @@
 package com.sordle.watpadsCardDeck.model
 
 import com.google.gson.JsonObject
-import jakarta.validation.constraints.NotBlank
 
 /**
  * Message sent in the play card phase to play provided card
@@ -9,7 +8,6 @@ import jakarta.validation.constraints.NotBlank
  * Syntax: {"card" : "[card]"}
  */
 data class PlayCardRequest (
-    @get:NotBlank
     val card : Cards
 ) {
     constructor(jsonObject: JsonObject) : this(
