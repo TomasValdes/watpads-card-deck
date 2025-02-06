8e7d34100d1065d9d35e3a0e5065ea183539b856
Tomas Valdes <tvaldes@wisc.edu>
2025-02-06 14:32:13 -0600

    Fixed issue with overlapping endpoints. Added handshake interceptor for future use.

--- src/main/kotlin/com/sordle/WatpadsCardDeck/config/WebSocketConfig.kt
+++ src/main/kotlin/com/sordle/WatpadsCardDeck/config/WebSocketConfig.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor
 
 @Configuration
 @EnableWebSocket
@@ -14,7 +15,9 @@ class WebSocketConfig(
 ) : WebSocketConfigurer {
 
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        registry.addHandler(gameWebSocketHandler, "/game")
+        registry.addHandler(gameWebSocketHandler, "/web/game")
+            // Interceptor to set initial session attributes, to be used for authorization later
+            .addInterceptors(HttpSessionHandshakeInterceptor())
             .setAllowedOrigins("http://localhost:3000")
     }
 }
