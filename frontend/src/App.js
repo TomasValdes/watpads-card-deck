36e8828466db65f6c25966a880eaa88af6051d55
Tomas Valdes <tvaldes@wisc.edu>
2025-02-16 18:23:14 -0600

    Added button to join game

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Game from "./Game";
+import {useState} from "react";
 
 function App() {
+    const [joinedGame, setJoinedGame] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <Game/>
+        {!joinedGame ? (
+          <button onClick={() => setJoinedGame(true)}>Join Game</button>
+        ) : (
+          <Game />
+        )}
       </header>
     </div>
   );
