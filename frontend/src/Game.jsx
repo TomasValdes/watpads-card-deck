902e6a2f456b38ecbae3cace7e2e07180f2b7719
Tomas Valdes <tvaldes@wisc.edu>
2025-02-24 12:54:26 -0600

    Added enum to handle gamestates, updated how playing cards is handled

--- frontend/src/Game.jsx
+++ frontend/src/Game.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import rockCard from './rockCard.png'
 import paperCard from './paperCard.png'
 import scissorsCard from './scissorsCard.png'
@@ -15,6 +15,35 @@ const Game = () => {
     const [userId, setUserId] = useState(null);
     const [userName, setUserName] = useState(null);
     const [winnerId, setWinner] = useState(null)
+    const [revealedCards, setRevealedCards] = useState(null);
+    const [trumpCard, setTrumpCard] = useState(null);
+    const [selfMove, setSelfMove] = useState();
+    const [opponentMove, setOpponentMove] = useState();
+    const cardTypes = {
+        Rock: rockCard,
+        Paper: paperCard,
+        Scissors: scissorsCard
+    };
+    const GameStates = Object.freeze({
+        SELECTING_TRUMP: Symbol("SelectingTrump"),
+        DRAFTING_CARDS: Symbol("DraftingCards"),
+        PLAYING_CARDS: Symbol("PlayingCards"),
+        REVEALING_CARDS: Symbol("RevealingCards"),
+        COMPLETED: Symbol("Completed")
+    });
+
+    const ResponseTypes = Object.freeze({
+        GAME: Symbol("GameResponse"),
+        PLAYER: Symbol("PlayerResponse"),
+        REVEAL: Symbol("RevealCardsResponse"),
+        USER: Symbol("UserResponse"),
+    });
+
+    const CardButton = ({cardName, onClick}) => (
+        <button onClick={() => onClick(cardName)}>
+            <img className="Card" src={cardTypes[cardName]} alt={`${cardName} Card`}/>
+        </button>
+    );
 
     useEffect(() => {
         const socket = new WebSocket("ws://localhost:8080/web/game");
@@ -27,22 +56,35 @@ const Game = () => {
             const data = JSON.parse(message.data);
             console.log("Received message:", data);
 
-            if (data.userId) {
-                setUserId(data.userId)
-                setUserName(data.userName)
-            }
 
-            if (data.gameState) {
-                setGameState(data.gameState);
-                setGameId(data.gameId);
+            switch (data.responseType){
+                case ResponseTypes.USER:
+                    setUserId(data.userId)
+                    setUserName(data.userName)
+                    break;
+                case ResponseTypes.GAME:
+                    setGameState(data.gameState);
+                    setGameId(data.gameId);
 
-                if (data.winner){
-                    setWinner(data.winner)
-                }
-            }
+                    if (data.winner) {
+                        setWinner(data.winner)
+                    }
 
-            if (data.hand) {
-                setHand(data.hand);
+                    /**
+                     * Make sure correct move is attributed to correct player
+                     */
+                    if (data.gameState === GameStates.REVEALING_CARDS){
+                        if (data.playerOneUserId === userId){
+                            setSelfMove(data.playerOneMove);
+                            setOpponentMove(data.playerTwoMove);
+                        } else{
+                            setSelfMove(data.playerTwoMove);
+                            setOpponentMove(data.playerOneMove);
+                        }
+                    }
+                    break;
+                case ResponseTypes.PLAYER:
+                    setHand(data.hand);
             }
         };
 
@@ -61,7 +103,10 @@ const Game = () => {
     };
 
     const selectTrump = (card) => {
-        sendMessage({ card });
+        if (!trumpCard) {
+            setTrumpCard(card)
+            sendMessage({card});
+        }
     };
 
     const addCardsToDeck = (card) => {
@@ -69,7 +114,7 @@ const Game = () => {
             const newCards = [...prevCards, card];
 
             if (newCards.length === 3) {
-                sendMessage({ card: newCards });
+                sendMessage({card: newCards});
             }
 
             return newCards;
@@ -77,64 +122,66 @@ const Game = () => {
     }
 
     const playCard = (card) => {
-        sendMessage({ card });
+        sendMessage({card});
     };
 
     return (
         <div>
-            <h1>Rock-Paper-Scissors Game</h1>
-            <p>User Name: {userName}</p>
-            <p>Game State: {gameState}</p>
-            {gameState === null && (
-                <div>
-                    <h1>Connecting to a game ⏱️</h1>
-                </div>
-            )}
-            {gameState === "SelectingTrump" && (
-                <div>
-                    <h2>Select a Trump Card</h2>
-                    <button onClick={() => selectTrump("Rock")}>
-                        <img className={"Card"} src={rockCard} alt={"Rock Card"}></img>
-                    </button>
-                    <button onClick={() => selectTrump("Paper")}>
-                        <img className={"Card"} src={paperCard} alt={"Paper Card"}></img>
-                    </button>
-                    <button onClick={() => selectTrump("Scissors")}>
-                        <img className={"Card"} src={scissorsCard} alt={"Scissors Card"}></img>
-                    </button>
-                </div>
-            )}
-            {
-                gameState === "DraftingCards" && (
+            <p style={{textAlign: 'left'}}>Trump Card:
+                {trumpCard &&
+                    <img className="Card" src={cardTypes[trumpCard]} alt={`${trumpCard} Card`}/>
+                }
+            </p>
+            <div className="Game-body">
+                <h1>Rock-Paper-Scissors Game</h1>
+                <p>Game State: {gameState}</p>
+                <p>User Name: {userName}</p>
+                <p>Cards revealed from deck: {revealedCards}</p>
+                {gameState === null && (
                     <div>
-                        <h2>Select three cards to add to the deck</h2>
-                        <button onClick={() => addCardsToDeck("Rock")}>
-                            <img className={"Card"} src={rockCard} alt={"Rock Card"}></img>
-                        </button>
-                        <button onClick={() => addCardsToDeck("Paper")}>
-                            <img className={"Card"} src={paperCard} alt={"Paper Card"}></img>
-                        </button>
-                        <button onClick={() => addCardsToDeck("Scissors")}>
-                            <img className={"Card"} src={scissorsCard} alt={"Scissors Card"}></img>
-                        </button>
+                        <h1>Connecting to a game ⏱️</h1>
+                    </div>
+                )}
+                {gameState === GameStates.SELECTING_TRUMP && (
+                    <div>
+                        <h2>{!trumpCard ? "Select a trump card"
+                            : "Waiting for opponent"}
+                        </h2>
+                        {Object.keys(cardTypes).map((cardName) => (
+                            <CardButton key={cardName} cardName={cardName} onClick={selectTrump}/>
+                        ))}
+                    </div>
+                )}
+                {gameState === GameStates.DRAFTING_CARDS && (
+                    <div>
+                        <h2>{selectedCards.length < 3 ? "Select three cards to add to the deck"
+                            : "Waiting for opponent"}
+                        </h2>
+                        {Object.keys(cardTypes).map((cardName) => (
+                            <CardButton key={cardName} cardName={cardName} onClick={addCardsToDeck}/>
+                        ))}
                     </div>
                 )
-            }
-            {gameState === "PlayingCards" && (
-                <div>
-                    <h2>Your Hand</h2>
-                    {hand.map((card, index) => (
-                        <button key={index} onClick={() => playCard(card)}>
-                            {card}
-                        </button>
-                    ))}
-                </div>
-            )}
-            {gameState === "Completed" && (
-                <div>
-                    <h2>{winnerId === userId ? "You win!" : "Better luck next time"}</h2>
-                </div>
-            )}
+                }
+                {gameState === GameStates.PLAYING_CARDS && (
+                    <div>
+                        <h2>Your Hand</h2>
+                        {hand.map((cardName, index) => (
+                            <CardButton key={index} cardName={cardName} onClick={playCard}/>
+                        ))}
+                    </div>
+                )}
+                {gameState === GameStates.REVEALING_CARDS && (
+                    <div>
+                        {revealPlays()}
+                    </div>
+                )}
+                {gameState === GameStates.COMPLETED && (
+                    <div>
+                        <h2>{winnerId === userId ? "You win!" : "Better luck next time"}</h2>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
