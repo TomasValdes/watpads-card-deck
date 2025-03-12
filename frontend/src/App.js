af172c2524d92e0aea1bd0df0cae3ae04d06930d
Tomas Valdes <tvaldes@wisc.edu>
2025-03-12 12:38:21 -0500

    Added vanta fog background

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,36 @@
 import './App.css';
 import Game from "./Game";
-import {useState} from "react";
+import {useEffect, useRef} from "react";
+import * as THREE from "three";
+import FOG from "vanta/dist/vanta.fog.min";
 
 function App() {
-    const [joinedGame, setJoinedGame] = useState(false);
+    const vantaRef = useRef(null);
+
+    useEffect(() => {
+        const effect = FOG({
+            el: vantaRef.current, // Attach to this div
+            THREE: THREE,
+            mouseControls: true,
+            touchControls: true,
+            gyroControls: false,
+            minHeight: 200.0,
+            minWidth: 200.0,
+            highlightColor: 0x44f45a,
+            midtoneColor: 0x0,
+            lowlightColor: 0x74ff74,
+            baseColor: 0x146814,
+        });
+
+        return () => {
+            if (effect) effect.destroy(); // Clean up effect on unmount
+        };
+    }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        {!joinedGame ? (
-          <button onClick={() => setJoinedGame(true)}>Join Game</button>
-        ) : (
+    <div className="App" ref={vantaRef}>
+      <header>
           <Game />
-        )}
       </header>
     </div>
   );
